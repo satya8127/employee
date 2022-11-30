16bbea2c151bef73766350a10e5b00a48aeb4ec4
satyakumartattukolla <satyakumar.tattukolla@nagarro.com>
2022-11-30 11:47:20 +0530

    second

---
+++ src/main/java/com/satya/employee/controller/HomeController.java
@@ -0,0 +1,43 @@
+package com.satya.employee.controller;
+
+import com.satya.employee.entity.Employeedb;
+import com.satya.employee.repository.EmployeeRepo;
+import com.satya.employee.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+
+public class HomeController
+{
+
+   @Autowired
+   private EmployeeService employeeService;
+
+   @Autowired
+   private EmployeeRepo employeeRepo;
+
+   @PostMapping("/save")
+   public ResponseEntity<Long> saveEmployee(@RequestBody Employeedb employeedb){
+
+      return new ResponseEntity<>(employeedb.getId(), HttpStatus.OK);
+
+   }
+   @PostMapping("/add")
+   public Employeedb addemployee(@RequestBody Employeedb employeedb){
+      return employeeRepo.save(employeedb);
+   }
+   @GetMapping("/find")
+
+   public List<Employeedb> findemployee(){
+      return  employeeRepo.findAll();
+   }
+
+}
